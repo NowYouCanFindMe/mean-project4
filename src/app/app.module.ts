6c28686f9888fece60aa06f06a538d4721cd35f6
NowYouCanFindMe <NowYouCanFindMe@users.noreply.github.com>
2018-11-12 18:47:45 -0500

    Files

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MatButtonModule, MatCardModule, MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { PageComponent } from './page/page.component';
+import { ContentService } from './shared/services/content.service';
+import { FullpageDirective } from './shared/directives/fullpage.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PageComponent,
+    FullpageDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule
+  ],
+  providers: [ContentService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
